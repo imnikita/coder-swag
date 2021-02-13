05133896f64c5d86dcedc54e07ba369fc70f8b6d
imnikita <imvpopov@gmail.com>
2021-02-13 18:36:49 +0200

    Collection View was added and customized.

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,14 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Nikita Popov on 13.02.2021.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
