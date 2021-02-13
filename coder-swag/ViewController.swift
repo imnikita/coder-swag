a8f44ffaa3968cb7603eed461a418c34a46ba01f
Nikita <imvpopov@gmail.com>
2021-02-13 10:14:30 +0200

    Initial Commit

---
+++ coder-swag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Nikita Popov on 13.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
